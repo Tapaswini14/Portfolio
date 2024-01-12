cfc54af36ec4b92d1fb16513ff03b3ef9d736adc
Tapaswini <tapaswini.rechargekit@gmail.com>
2024-01-12 15:42:15 +0530

    change

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
- 
   model: any = {};
 
-  constructor(){}
+  constructor() {}
 
-  ngOnInit() {
-     }
+  ngOnInit() {}
 
-
+  onClick(): void {
+  }
 }
